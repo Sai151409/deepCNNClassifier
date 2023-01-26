6ef744bd5e278b728d514f751b35521ba8f7bfa7
Sai151409 <sairamnaidu694@gmail.com>
2023-01-26 19:37:53 +0530

    init setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
-with open('README.md', 'r', encoding='utf-8') as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = '0.0.0'
+__version__ = "0.0.0"
 
-REPO_NAME = 'deepCNNClassifer'
-AUTHOR_USER_NAME = 'Sai151409'
-SRC_REPO = 'deepClassifier'
-AUTHOR_EMAIL = 'sairamnaidu694@gmail.com'
+REPO_NAME = "deepCNNClassifer"
+AUTHOR_USER_NAME = "Sai151409"
+SRC_REPO = "deepClassifier"
+AUTHOR_EMAIL = "sairamnaidu694@gmail.com"
 
 
 setuptools.setup(
@@ -16,13 +16,13 @@ setuptools.setup(
     version=__version__,
     author = AUTHOR_USER_NAME,
     author_email = AUTHOR_EMAIL,
-    description='This is small python package for CNN App',
+    description="This is small python package for CNN App",
     long_description=long_description,
-    long_description_content = 'text/markdown', 
+    long_description_content = "text/markdown", 
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={'':'src'},
-    packages=setuptools.find_packages(where='src')
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
 )
