54aea4c36dd867d3596de112d5bcd85351ca37f0
Sai151409 <sairamnaidu694@gmail.com>
2023-01-28 21:02:39 +0530

    unit tested

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -2,14 +2,21 @@ import pytest
 from deepClassifier.utils import read_yaml_file
 from pathlib import Path
 from box import ConfigBox
+from ensure.main import EnsureError
 
 
-yaml_files = [
-    "tests/data/empty.yaml",
-    "tests/data/demo.yaml"]
+class Test_read_yaml:
+    yaml_files = ["tests/data/empty.yaml", "tests/data/demo.yaml"]
 
-def test_read_yaml_empty():
-    with pytest.raises(ValueError):
-        read_yaml_file(Path(yaml_files[0]))
-        
+    def test_read_yaml_empty(self):
+        with pytest.raises(ValueError):
+            read_yaml_file(Path(self.yaml_files[0]))
 
+    def test_read_yaml_return_type(self):
+        response = read_yaml_file(Path(self.yaml_files[1]))
+        assert isinstance(response, ConfigBox)
+
+    @pytest.mark.parametrize("path_to_yaml_file", yaml_files)
+    def test_read_yaml_bad_type(self, path_to_yaml_file):
+        with pytest.raises(EnsureError):
+            read_yaml_file(path_to_yaml_file)
