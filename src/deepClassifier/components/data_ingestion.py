c42845969870d8c24889c312863be1dcfb603ae7
Sai151409 <sairamnaidu694@gmail.com>
2023-01-28 21:03:05 +0530

    small changes in code

--- src/deepClassifier/components/data_ingestion.py
+++ src/deepClassifier/components/data_ingestion.py
@@ -1,40 +1,48 @@
 import os
 import urllib.request as request
 from zipfile import ZipFile
-from deepClassifier.entity import *
-from deepClassifier import *
+from deepClassifier.entity import DataIngestionConfig
+from deepClassifier import logger
 from deepClassifier.utils import get_size
 from pathlib import Path
 from tqdm import tqdm
+
 
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
 
     def download_file(self):
-        logger.info('Trying to download the file')
+        logger.info("Trying to download the file")
         if not os.path.exists(self.config.local_data_file):
-            logger.info('Download started.........')
+            logger.info("Download started.........")
             filename, headers = request.urlretrieve(
-                url = self.config.source_URL,
-                filename = self.config.local_data_file
+                url=self.config.source_URL, filename=self.config.local_data_file
             )
-            logger.info(f'{filename} download! with following info : \n{headers}')
+            logger.info(f"{filename} download! with following info : \n{headers}")
         else:
-            logger.info(f"File already exists of size : {get_size(Path(self.config.local_data_file))}")
-            
+            logger.info(
+                f"File already exists of size : {get_size(Path(self.config.local_data_file))}"
+            )
+
     def _get_updated_list_of_files(self, list_of_files):
-        return [f for f in list_of_files if f.endswith(".jpg") and ("Cat" in f or "Dog" in f)]
+        return [
+            f
+            for f in list_of_files
+            if f.endswith(".jpg") and ("Cat" in f or "Dog" in f)
+        ]
 
     def _preprocess(self, zf: ZipFile, f: str, working_dir: str):
         target_filepath = os.path.join(working_dir, f)
         if not os.path.exists(target_filepath):
             zf.extract(f, working_dir)
-        
+
         if os.path.getsize(target_filepath) == 0:
-            logger.info(f"removing file : {target_filepath} of size : {get_size(Path(target_filepath))}")
+            logger.info(
+                f"removing file : {target_filepath} of size : {get_size(Path(target_filepath))}"
+            )
             os.remove(target_filepath)
-            
+
     def unzip_and_clean(self):
         logger.info("unzipping file and removing the unwanted file")
         with ZipFile(file=self.config.local_data_file, mode="r") as zf:
--- src/deepClassifier/utils/common.py
+++ src/deepClassifier/utils/common.py
@@ -33,7 +33,8 @@ def read_yaml_file(file_path : Path) -> ConfigBox:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e 
-    
+
+   
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose = True):
     """create list of directories
@@ -46,9 +47,10 @@ def create_directories(path_to_directories: list, verbose = True):
         os.makedirs(path, exist_ok=True)
         if verbose:
             logger.info(f"created directory at : {path}")
+
             
 @ensure_annotations
-def save_json(path:Path, data : dict):
+def save_json(path: Path, data : dict):
     """save json data
 
     Args:
@@ -56,17 +58,16 @@ def save_json(path:Path, data : dict):
         data (dict): data to be saved in json file
     """
     with open(file=path, mode='w') as f:
-        json.dump(data. f, indent=4)
-    
+        json.dump(data, f, indent=4)
     logger.info(f"json file saved at : {path}")
+ 
     
 @ensure_annotations
-def load_json(path:Path) -> ConfigBox:
+def load_json(path: Path) -> ConfigBox:
     """load the json file
 
     Args:
-        path (str): json  file path
-        
+        path (str): json  file path   
     Returns:
         ConfigBox : data as class attributes instead of dict
     """
@@ -75,8 +76,9 @@ def load_json(path:Path) -> ConfigBox:
     logger.info("json file loaded successfully from : {path}")
     return ConfigBox(content)
 
+
 @ensure_annotations
-def save_bin(data : Any, path : Path):
+def save_bin(data: Any, path: Path):
     """save binary file
 
     Args:
@@ -85,9 +87,10 @@ def save_bin(data : Any, path : Path):
     """
     joblib.dump(value=data, filename=path)
     logger.info(f"binary file saved at {path}")
+ 
     
 @ensure_annotations
-def load_bin(path:Path) -> Any:
+def load_bin(path: Path) -> Any:
     """load binary data
 
     Args:
@@ -100,8 +103,9 @@ def load_bin(path:Path) -> Any:
     logger.info(f"binary file loaded from : {path}")
     return data
 
+
 @ensure_annotations
-def get_size(path:Path) -> str:
+def get_size(path: Path) -> str:
     """get size in KB
 
     Args:
