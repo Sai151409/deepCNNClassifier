65e3c15620cea0c5bee078cae6013783c0e1693b
Sai151409 <sairamnaidu694@gmail.com>
2023-01-30 13:33:20 +0530

    training updtaed

--- src/deepClassifier/pipeline/stage_03_training.py
+++ src/deepClassifier/pipeline/stage_03_training.py
@@ -8,11 +8,11 @@ STAGE_NAME = 'Training Stage'
 def main():
     config = ConfigurationManager()
     training_config = config.get_training_config()
-    prepare_callbacks_config = config.get_prepare_callback_config
+    prepare_callbacks_config = config.get_prepare_callback_config()
     prepare_callbacks = PrepareCallBackModel(config=prepare_callbacks_config)
     callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
 
-    training = TrainingModel(config=training)
+    training = TrainingModel(config=training_config)
     training.get_base_model()
     training.train_valid_generator()
     training.train(callback_list=callback_list)
